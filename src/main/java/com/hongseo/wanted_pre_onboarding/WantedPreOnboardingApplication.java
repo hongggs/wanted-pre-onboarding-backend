050ffa5b46f86e3a592d52f898211c86c79e72af
hongggs <hongseoda@gmail.com>
2024-07-28 15:04:36 +0900

    chore: spring boot를 이용하여 프로젝트 초기 세팅

---
+++ src/main/java/com/hongseo/wanted_pre_onboarding/WantedPreOnboardingApplication.java
@@ -0,0 +1,13 @@
+package com.hongseo.wanted_pre_onboarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WantedPreOnboardingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WantedPreOnboardingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hongseo/wanted_pre_onboarding/WantedPreOnboardingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hongseo.wanted_pre_onboarding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WantedPreOnboardingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
