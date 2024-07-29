9e74ae916a0afad59bec1667885d2012be1a607f
hongggs <hongseoda@gmail.com>
2024-07-29 23:17:04 +0900

    fix: BaseTimeEntity를 위해 @EnableJpaAuditing 추가

--- src/main/java/com/hongseo/wanted_pre_onboarding/WantedPreOnboardingApplication.java
+++ src/main/java/com/hongseo/wanted_pre_onboarding/WantedPreOnboardingApplication.java
@@ -2,8 +2,10 @@ package com.hongseo.wanted_pre_onboarding;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class WantedPreOnboardingApplication {
 
 	public static void main(String[] args) {
